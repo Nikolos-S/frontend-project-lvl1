16f74ff563dbf2ce8fbc2e491515c2f32439638c
Nikolos-S <sarkozzy@mail.ru>
2022-03-29 15:18:15 +0700

    redacted game-even,simplified verification

--- games/EvenNumberGame.js
+++ games/EvenNumberGame.js
@@ -9,25 +9,20 @@ const userResponse = () => {
     const randomNumber = generatorRandom(0, 100); // рандомное число
     console.log(`Question: ${randomNumber}`); // вывод рандомного числа на экран
     const answer = readlineSync.question('Your answer: '); // вывод ответа игрока на экран
+    let rightAnswer;
     if (randomNumber % 2 === 0) {
-      if (answer === 'yes') {
-        console.log('Correct!');
-      } else {
-        console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'`);
-        console.log(`Let's try again, ${name}!`);
-        return;
-      }
+      rightAnswer = 'yes';
+    } else {
+      rightAnswer = 'no';
     }
-    if (randomNumber % 2 !== 0) {
-      if (answer === 'no') {
-        console.log('Correct!');
-      } else {
-        console.log(`${answer} is wrong answer ;(. Correct answer was 'no'`);
-        console.log(`Let's try again, ${name}!`);
-        return;
-      }
+    if (rightAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'`);
+      console.log(`Let's try again, ${name}!`);
+      return;
     }
+    console.log(`Congratulations, ${name}`);
   }
-  console.log(`Congratulations, ${name}`);
 };
 export default userResponse;
