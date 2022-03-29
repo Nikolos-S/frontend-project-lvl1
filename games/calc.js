f6451f11cd8edd363b797fb28f83024b4a5fac27
Nikolos-S <sarkozzy@mail.ru>
2022-03-29 18:31:05 +0700

    delete index in calc

--- games/calc.js
+++ games/calc.js
@@ -9,8 +9,7 @@ const resultsСomparer = () => {
     const num1 = generatorRandom(0, 11); // первое число
     const num2 = generatorRandom(0, 11); // второе число
     const randomOperator = ['+', '-', '*']; // массив с рандомными операторами
-    const index = generatorRandom(0, 3);
-    const operator = randomOperator[index]; // рандомный оператор
+    const operator = randomOperator[generatorRandom(0, 3)]; // рандомный оператор
     console.log(`Question: ${num1} ${operator} ${num2}`);
     const answer = Number(readlineSync.question('Your answer: '));
     const calculator = (op) => {
