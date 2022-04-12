69383e3b34c9b291ced4b6df9f4d660a6a350fb3
Nikolos-S <sarkozzy@mail.ru>
2022-04-12 13:18:43 +0700

    fixed progression Game

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,23 +3,20 @@ import generateRandomNumber from '../randomNumber.js';
 
 const gameTask = 'What number is missing in the progression?';
 
-const generateProgression = (startNumber, step, hidden, lenghtProgression) => {
+const generateProgression = (startNumber, step, hidden, progressionLength) => {
   const result = [];
-  for (let i = 0; i < lenghtProgression; i += 1) {
-    if (i !== hidden) {
-      result.push(startNumber + step * i);
-    } else {
-      result.push('..');
-    }
+  for (let i = 0; i < progressionLength; i += 1) {
+    result.push(i === hidden ? '..'
+      : startNumber + step * i);
   }
   return result;
 };
 const findMissingNumber = () => {
-  const lenght = generateRandomNumber(5, 11);
-  const hiddenElement = generateRandomNumber(5, lenght - 1);
+  const length = generateRandomNumber(5, 11);
+  const hiddenElement = generateRandomNumber(5, length - 1);
   const stepProgression = generateRandomNumber(1, 8);
   const firstNumber = generateRandomNumber(1, 50);
-  const progression = generateProgression(firstNumber, stepProgression, hiddenElement, lenght);
+  const progression = generateProgression(firstNumber, stepProgression, hiddenElement, length);
   const question = progression.join(' ');
   const correctAnswer = firstNumber + stepProgression * hiddenElement;
   return [question, String(correctAnswer)];
